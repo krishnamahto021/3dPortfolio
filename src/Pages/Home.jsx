d8925ff2d30b575ff673249390d93041cdcf4f8b
krishna mahto <mahtok98212@gmail.com>
2024-01-14 10:41:52 +0530

    Add the sound for realisitic behaviour of the island

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import { Canvas } from "@react-three/fiber";
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import Loader from "../Components/Loader";
 import Island from "../Models/Island";
 import Sky from "../Models/Sky";
 import Plane from "../Models/Plane";
 import Bird from "../Models/Bird";
 import HomeInfo from "../Components/HomeInfo";
+import sakura from "../assets/sakura.mp3";
+import { soundoff, soundon } from "../assets/icons";
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
   const adjustIslandForScreenSize = () => {
@@ -39,6 +45,15 @@ const Home = () => {
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
+  useEffect(() => {
+    if (isPlayingMusic) {
+      audioRef.current.play();
+    }
+    return () => {
+      audioRef.current.pause();
+    };
+  }, [isPlayingMusic]);
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-9 left-0 right-0 z-10 flex items-center justify-center p-2 mt-2">
@@ -72,6 +87,17 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
+      <div className="absolute bottom-2 left-2">
+        <img
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="w-10 h-10 object-contain cursor-pointer"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        ></img>
+      </div>
+      <div className="absolute right-2 bottom-2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-xl p-2 md:text-lg cursor-wait max-w-[40%] capitalize text-sm">
+        hold the screen and move your mouse to explore more
+      </div>
     </section>
   );
 };
